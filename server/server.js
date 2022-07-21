b5cd5d26a0d13a144f5d880f36685ea86c8f0122
Glen Yang <gleny4001@gmail.com>
2022-07-21 02:57:15 -0700

    connect to server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,19 @@
 const express = require('express');
+const port = 4000;
 const app = express(); 
+const cors = require('cors');
 
-app.listen(5000, ()=>{console.log('listening on port 5000')});
+app.use(express.urlencoded({extended: false}));
+app.use(cors());
+app.use(express.json());
+
+app.get("/", cors(), (req, res) => {
+  res.send("working")});
+
+app.post("/formData", (req, res) =>{
+  const newData = req.body;
+  console.log(newData);
+  
+})
+
+app.listen({port}, ()=>{console.log('listening on port ' + port)});
